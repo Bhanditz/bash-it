77b19f863531bfc3d479bb0792e9f3399893833b
Michael Taboada <ai5hf@hotmail.com>
2015-12-09 22:31:33 -0500

    Woo! It works!

--- bash-it.cpp
+++ bash-it.cpp
@@ -4,9 +4,15 @@
 
 int main()
 {
+al_init();
+al_install_audio();
+al_install_keyboard();
+al_init_acodec_addon();
+ALLEGRO_DISPLAY* disp = al_create_display(640,480);
 // Generate the main menu.
 string options[] = {"play","exit"};
 dynamic_menu* mainMenu = create_menu(options, 2, NULL, 0);
+mainMenu->set_display(disp);
 int r = mainMenu->run_extended("", "", 1, true);
 if (r == 1)
 {
