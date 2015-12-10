343ec0565e230e39fca7adf2e470395e16d47c97
Michael Taboada <ai5hf@hotmail.com>
2015-12-09 21:52:52 -0500

    Fixed syntax errors.

---
+++ comp.sh
@@ -0,0 +1,1 @@
+g++ -I/usr/include/speech-dispatcher -Igame-kit -I game-kit/allegro_stuff -Igame-kit/screen-reader game-kit/allegro_stuff/dynamic_menu.cpp game-kit/allegro_stuff/sound.cpp game-kit/screen-reader/screen_reader.cpp game-kit/menu_helper.cpp bash-it.cpp -lallegro -lallegro_audio -lallegro_acodec -lallegro_ttf -lallegro_font -lspeechd
