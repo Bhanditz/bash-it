51cdfe5931f41ac4c5159fc6d1e011f0a55b06fa
Michael Taboada <ai5hf@hotmail.com>
2015-12-12 01:10:40 -0500

    Updated game-kit

--- bash-it.cpp
+++ bash-it.cpp
@@ -1,9 +1,14 @@
 #include <game.h>
 #include <menu_helper.h>
+#include <string>
+#include <vector>
+#include <allegro5/allegro.h>
+using std::string;
+using std::vector;
 
 int main()
 {
-game-window("Bash It!")
+ALLEGRO_DISPLAY* disp = game_window("Bash It!");
 // Generate the main menu.
 string options[] = {"play","exit"};
 dynamic_menu* mainMenu = create_menu(options, 2, NULL, 0);
@@ -11,14 +16,15 @@ mainMenu->set_display(disp);
 int r = mainMenu->run_extended("", "", 1, true);
 if (r == 1)
 {
-vector<string>* soundpackList = function();
-string array[soundpackList->size()];
+vector<string>* soundpackList = get_dir_children("sounds", 2);
+string array[soundpackList->size()+1];
 for (int x = 0; x < soundpackList->size(); x++)
 {
 array[x] = (*soundpackList)[x];
 }
-dynamic_menu* soundpackMenu = create_menu(soundpackList, soundpackList->size(), NULL, 0);
+array[soundpackList->size()] = "Back to main menu.";
+dynamic_menu* soundpackMenu = create_menu(array, soundpackList->size()+1, NULL, 0);
 soundpackMenu->set_display(disp);
-int r = soundpackMenu->run_extended("", "", 1, true);
+r = soundpackMenu->run_extended("", "", 1, true);
 }
 }
