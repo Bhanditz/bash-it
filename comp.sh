8620540f8043a83b66c1543cf2b8e9b475b3e7d5
Storm Dragon <stormdragon2976@gmail.com>
2017-04-08 09:48:01 -0400

    comp.sh now compiles to the file named bash-it.

--- comp.sh
+++ comp.sh
@@ -1,1 +1,3 @@
-g++ -ggdb -I/usr/include/speech-dispatcher -Igame-kit -Igame-kit/allegro_stuff -Igame-kit/screen-reader game-kit/allegro_stuff/keyboard.cpp game-kit/allegro_stuff/dynamic_menu.cpp game-kit/allegro_stuff/sound.cpp game-kit/screen-reader/screen_reader.cpp game-kit/menu_helper.cpp game-kit/misc.cpp game-kit/soundplayer.cpp play.cpp bash-it.cpp -lallegro -lallegro_audio -lallegro_acodec -lallegro_ttf -lallegro_font -lspeechd
+#!/bin/bash
+
+g++ -ggdb -I/usr/include/speech-dispatcher -Igame-kit -Igame-kit/allegro_stuff -Igame-kit/screen-reader game-kit/allegro_stuff/keyboard.cpp game-kit/allegro_stuff/dynamic_menu.cpp game-kit/allegro_stuff/sound.cpp game-kit/screen-reader/screen_reader.cpp game-kit/menu_helper.cpp game-kit/misc.cpp game-kit/soundplayer.cpp play.cpp game.cpp -lallegro -lallegro_audio -lallegro_acodec -lallegro_ttf -lallegro_font -lspeechd && mv a.out bash-it
